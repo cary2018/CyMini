03b4c2faf0ca3d777cc90cbca7a34bf5ea9d7d55
cary <373889161@qq.com>
2024-06-12 22:26:27 +0800

    模板更新

--- app/index/controller/Navigation.php
+++ app/index/controller/Navigation.php
@@ -14,17 +14,20 @@
 namespace app\index\controller;
 
 use app\index\BaseController;
+use think\facade\View;
 
 class Navigation extends BaseController
 {
     public function index(){
-        $id = request()->param('id');
-        if($id){
-            $data = FindTable('category',[['id','=',$id],['isShow','=',1]]);
-
+        $pass = request()->param('pass');
+        if($pass == '123..'){
+            SetSe('access_navigation',$pass);
         }else{
-
+            $data = array();
+            $msg = ['code'=>300,'message'=>'访问密码错误！','data'=>$data,'acc'=>$pass];
         }
+        $acc =  GetSe('access_navigation');
+        View::assign('pass',$acc);
         return View();
     }
 }
